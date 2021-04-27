98d54682d38a00125a5890aa3b4cd9027a1f6734
NewUser <newuser@example.com>
2021-04-27 11:22:18 +0300

    first commit

--- investpro/settings.py
+++ investpro/settings.py
@@ -14,10 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'investapp/templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -60,7 +58,7 @@ ROOT_URLCONF = 'investpro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,31 +78,17 @@ WSGI_APPLICATION = 'investpro.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-# Password validation
 
-#DATABASES = {
-#'default': {
-#'ENGINE': 'django.db.backends.mysql',
-#'NAME': 'django',
-#'USER': 'root',
-#'PASSWORD': '',
-#'HOST': '',
-#'PORT': '',
-#'OPTIONS': {
-#'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#}
-#}
-#}
- 
- # Password validation
+# Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -161,12 +145,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Media paths
 
 # Base url to serve media files
-#MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 # Path where media is stored
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
